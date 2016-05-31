d96db57428ad06ebc3a2f6f44de6babd552b9b7a
Kyle Hamburg <kylelhamburg@gmail.com>
2016-05-31 15:54:37 -0600

    Added GET, POST, PUT, and DELETE function

---
+++ dogs.js
@@ -0,0 +1,8 @@
+module.exports = [{
+  name:"Roach",
+  breed:"Horse"
+},
+{
+  name:"Doug",
+  breed:"Dachshund"
+}]
---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var dogs = require('./dogs.js');
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/dogs', function(req, res, next){
+  res.send(dogs);
+});
+
+app.post('/dogs', function(req, res, next){
+  dogs.push(req.body);
+  res.send(dogs);
+});
+
+app.put('/dogs/:id', function(req, res, next){
+  for(var i = 0; i < dogs.length; i++){
+    if(req.params.id==dogs[i].name){
+      dogs[i] = req.body;
+    }
+  }
+  res.send(dogs);
+});
+
+app.delete('/dogs/:id', function(req,res,next){
+  for(var i = 0; i < dogs.length; i++){
+    if(req.params.id==dogs[i].name){
+      dogs.splice(i, 1);
+    }
+  }
+  res.send(dogs);
+});
+
+app.listen(8080, function(){
+  console.log("This is a server");
+});
